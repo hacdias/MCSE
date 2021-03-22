8c0636595ff86d34bee8e612004a52ca4815b45d
v777v <38751851+v777v@users.noreply.github.com>
2021-03-22 07:34:56 +0100

    part2 started

--- bst.v
+++ bst.v
@@ -1,4 +1,6 @@
 Require Import Arith.
+Require Import List.
+Require Import Lia.
 
 (*
   match authors with
@@ -117,19 +119,172 @@ Qed.
 Fixpoint occurs (n: nat) (t: tree) : Prop :=
   match t with
   | leaf => False
-  | node l v r =>
-      match n ?= v with
-      | Eq => True
-      | Lt => occurs n l
-      | Gt => occurs n r
-      end
+  | node l v r => (v = n) \/ (occurs n l) \/ (occurs n r)
   end.
+
+Lemma insert_occurs : forall (t: tree) (n: nat) (m: nat), occurs n (insert m t) <-> n=m \/ occurs n t.
+Proof.
+intros.
+unfold iff.
+split.
+
+intros.
+induction t.
+simpl.
+simpl in H. 
+intuition.
+simpl.
+simpl in H.
+case (m?=n0) eqn:C1.
+apply nat_compare_eq in C1.
+intuition.
+simpl in H.
+intuition.
+simpl in H.
+intuition.
+
+intros.
+destruct H.
+induction t.
+simpl.
+intuition.
+simpl.
+case (m?=n0) eqn:C1.
+apply nat_compare_eq in C1.
+simpl.
+subst.
+intuition.
+simpl.
+intuition.
+simpl.
+intuition.
+
+
+induction t.
+simpl.
+intuition.
+simpl in H.
+intuition.
+simpl.
+case (m?=n0) eqn:C1.
+apply nat_compare_eq in C1.
+simpl.
+auto.
+simpl.
+left.
+auto.
+simpl.
+left.
+auto.
+
+simpl.
+case (m?=n0) eqn:C1.
+apply nat_compare_eq in C1.
+simpl.
+auto.
+simpl.
+right.
+left.
+auto.
+simpl.
+intuition.
+
+simpl.
+case (m?=n0) eqn:C1.
+apply nat_compare_eq in C1.
+simpl.
+auto.
+simpl.
+intuition.
+simpl.
+intuition.
+Qed.
+
+Lemma list_occurs : forall (t: tree) (n: nat), occurs n t <-> In n (tree_to_list t).
+Proof.
+intros.
+unfold iff.
+split.
+
+intros.
+induction t.
+auto.
+simpl.
+simpl in H.
+intuition.
+apply in_or_app.
+right.
+left.
+auto.
+
+intros.
+induction t.
+auto.
+simpl.
+simpl in H.
+apply in_app_or in H.
+destruct H. 
+right.
+left.
+intuition.
+destruct H.
+auto.
+right.
+right.
+intuition.
+Qed.
+
+Lemma bst_occurs : forall (l: list nat) (n: nat), In n l <-> occurs n (list_to_bst l).
+Proof.
+intros.
+unfold iff.
+split.
+
+intros.
+induction l.
+auto.
+simpl.
+simpl in H.
+intuition.
+apply insert_occurs.
+intuition.
+apply insert_occurs.
+intuition.
+
+intros.
+induction l.
+auto.
+simpl.
+simpl in H.
+apply insert_occurs in H.
+intuition.
+Qed.
 
 (* Proves that the sorted version of a tree contains the same elements as the original one. *)
 Lemma sorted_occurs : forall (t: tree) (n: nat), occurs n t <-> occurs n (sort t).
 Proof.
-  (* TODO *)
-Admitted.
+intros.
+unfold iff.
+split.
+
+intros.
+induction t.
+auto.
+unfold sort.
+apply bst_occurs.
+apply list_occurs.
+simpl.
+auto.
+
+intros.
+induction t.
+auto.
+unfold sort in H.
+apply bst_occurs in H.
+apply list_occurs in H.
+simpl.
+auto.
+Qed.
 
 (* PART 2 *)
 
@@ -149,11 +304,184 @@ Fixpoint treeMin (t: tree): option nat :=
 prove correctness of the treeMin function, i.e. prove that:
 – the minimal element belongs to the tree and
 – that the values in all nodes are greater or equal than the minimal value.  *)
-Lemma treeMin_correct: forall (t: tree) (n: nat), 
-  treeMin t = Some n -> occurs n t /\ tree_forall (fun y => y <= n) t.
+Lemma treeMin_correct1: forall (t: tree) (n: nat), 
+  treeMin t = Some n -> occurs n t.
 Proof.
-  (* TODO *)
-Admitted.
+intros.
+
+induction t.
+simpl in H.
+congruence.
+
+simpl.
+simpl in H.
+case (treeMin t1) eqn:C1.
+case (treeMin t2) eqn:C2.
+case (n1?=n2) eqn:C3.
+
+apply nat_compare_eq in C3.
+case (n0?=n1) eqn:C4.
+
+apply nat_compare_eq in C4.
+subst.
+right.
+right.
+apply IHt2.
+inversion H.
+f_equal.
+symmetry.
+apply Nat.min_l.
+Nat.solve_min.
+
+apply nat_compare_lt in C4.
+subst.
+left.
+inversion H.
+symmetry.
+apply Nat.min_l.
+Nat.solve_min.
+
+apply nat_compare_gt in C4.
+subst.
+right.
+right.
+apply IHt2.
+inversion H.
+f_equal.
+replace (Init.Nat.min n2 n2) with n2.
+symmetry.
+Nat.solve_min.
+Nat.solve_min.
+
+apply nat_compare_lt in C3.
+case (n0?=n1) eqn:C4.
+
+apply nat_compare_eq in C4.
+subst.
+right.
+left.
+apply IHt1.
+inversion H.
+f_equal.
+symmetry.
+apply Nat.min_l.
+Nat.solve_min.
+
+apply nat_compare_lt in C4.
+left.
+inversion H.
+symmetry.
+apply Nat.min_l.
+Nat.solve_min.
+
+apply nat_compare_gt in C4.
+right.
+left.
+apply IHt1.
+inversion H.
+f_equal.
+replace (Init.Nat.min n1 n2) with n1.
+symmetry.
+Nat.solve_min.
+Nat.solve_min.
+
+apply nat_compare_gt in C3.
+case (n0?=n2) eqn:C4.
+
+apply nat_compare_eq in C4.
+subst.
+right.
+right.
+apply IHt2.
+inversion H.
+f_equal.
+symmetry.
+apply Nat.min_l.
+replace (Init.Nat.min n1 n2) with n2.
+auto.
+symmetry.
+Nat.solve_min.
+
+apply nat_compare_lt in C4.
+left.
+inversion H.
+symmetry.
+apply Nat.min_l.
+replace (Init.Nat.min n1 n2) with n2.
+auto with arith.
+symmetry.
+Nat.solve_min.
+
+apply nat_compare_gt in C4.
+right.
+right.
+apply IHt2.
+inversion H.
+f_equal.
+replace (Init.Nat.min n1 n2) with n2.
+symmetry.
+Nat.solve_min.
+symmetry.
+Nat.solve_min.
+
+case (n0?=n1) eqn:C3.
+
+apply nat_compare_eq in C3.
+subst.
+right.
+left.
+apply IHt1.
+inversion H.
+f_equal.
+symmetry.
+Nat.solve_min.
+
+apply nat_compare_lt in C3.
+left.
+inversion H.
+symmetry.
+Nat.solve_min.
+
+apply nat_compare_gt in C3.
+right.
+left.
+apply IHt1.
+inversion H.
+f_equal.
+symmetry.
+Nat.solve_min.
+
+case (treeMin t2) eqn:C2.
+case (n0?=n1) eqn:C3.
+
+apply nat_compare_eq in C3.
+subst.
+right.
+right.
+apply IHt2.
+inversion H.
+f_equal.
+Nat.solve_min.
+
+apply nat_compare_lt in C3.
+left.
+inversion H.
+symmetry.
+Nat.solve_min.
+
+apply nat_compare_gt in C3.
+right.
+right.
+apply IHt2.
+inversion H.
+f_equal.
+symmetry.
+Nat.solve_min.
+
+left.
+inversion H.
+auto.
+Qed.
 
 (* Define a function leftmost that given a tree will return a value of its leftmost
 node. *)
