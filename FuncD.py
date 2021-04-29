f429375228df659ff5280caa4235ed963046ac8d
Nimo Beeren <nimobeeren@gmail.com>
2021-04-29 15:33:58 +0200

    read csv data and set the schema

---
+++ FuncD.py
@@ -0,0 +1,35 @@
+# %%
+from pyspark.sql import SparkSession
+from pyspark.sql.types import DecimalType, IntegerType, StringType, StructField, StructType, TimestampType
+
+# %%
+USERS_DATA_PATH = "data/subset_users.csv"
+
+# %%
+spark = SparkSession.builder.appName("FuncD").getOrCreate()
+
+# %%
+# This reflects the SQL schema of the `users` table provided at:
+# https://github.com/gousiosg/github-mirror/blob/3d5f4b2ffa5d510455e58b1209c31f4d1b211306/sql/schema.sql
+schema = StructType([
+    StructField("id", IntegerType()),
+    StructField("login", StringType()),
+    StructField("company", StringType()),
+    StructField("created_at", TimestampType()),
+    StructField("type", StringType()),
+    StructField("fake", IntegerType()),
+    StructField("deleted", IntegerType()),
+    StructField("long", DecimalType(11, 8)),
+    StructField("lat", DecimalType(10, 8)),
+    StructField("country_code", StringType()),
+    StructField("state", StringType()),
+    StructField("city", StringType()),
+    StructField("location", StringType()),
+])
+users = spark.read.csv(USERS_DATA_PATH, schema, nullValue='\\N')
+
+# Print some results to see if it worked
+print(users.head(3))
+
+# %%
+spark.stop()
