65c9835f88866b830e5ec376836d81ec47430b90
Nimo Beeren <nimobeeren@gmail.com>
2021-05-13 10:32:30 +0200

    create FunctionalDependency class

--- FuncD.py
+++ FuncD.py
@@ -1,8 +1,8 @@
-# %% Imports
+# %%
 import csv
 from itertools import chain, combinations
 from operator import add
-from typing import Any, Tuple
+from typing import Any
 
 from iso3166 import countries
 from pyspark.sql import SparkSession
@@ -10,12 +10,6 @@ from pyspark.sql.functions import udf
 from pyspark.sql.types import (DecimalType, IntegerType, StringType,
                                StructField, StructType, TimestampType)
 
-# Type alias
-# Uses `Tuple[]` because `tuple[]` is only supported since Python 3.9
-# See: https://docs.python.org/3/library/typing.html#typing.Tuple
-FunctionalDependency = Tuple[Tuple[str, ...], str]
-
-# %% Configuration
 SOFT_THRESHOLD = 0.9
 # USERS_DATA_PATH = "data/users.csv"
 USERS_DATA_PATH = "data/subset_users.csv"
@@ -30,7 +24,10 @@ IGNORED_ATTRIBUTES = {
   'lat'
 }
 
-# %% Mapping and reducing functions
+class FunctionalDependency:
+  def __init__(self, lhs: 'tuple[str, ...]', rhs: str):
+    self.lhs = lhs
+    self.rhs = rhs
 
 def attrs_to_tuple(lhs_attrs: 'tuple[str, ...]', rhs_attr: str):
   """
@@ -44,8 +41,8 @@ def attrs_to_tuple(lhs_attrs: 'tuple[str, ...]', rhs_attr: str):
 
 
 def tuple_to_dict(tup: 'tuple[FunctionalDependency, int]') -> 'tuple[Any, dict[str, int]]':
-  (lhs, rhs), count = tup
-  return (lhs, {rhs: count})
+  fd, count = tup
+  return (fd.lhs, {fd.rhs: count})
 
 
 def counts_to_prob(values: 'tuple[Any, dict[str, int]]'):
@@ -75,13 +72,13 @@ def counts_to_prob(values: 'tuple[Any, dict[str, int]]'):
   }
 
 
-def dependency_prob(lhs_cols: 'tuple[str, ...]', rhs_col: str):
+def dependency_prob(fd: FunctionalDependency):
   """
   Computes the probablity that two records with the same values for the LHS
   attributes have the same RHS value.
   """
   # Count RHS values by LHS value
-  rdd = users.rdd.map(attrs_to_tuple(lhs_cols, rhs_col))
+  rdd = users.rdd.map(attrs_to_tuple(fd.lhs, fd.rhs))
   rdd = rdd.reduceByKey(add)
   rdd = rdd.map(tuple_to_dict)
 
@@ -118,7 +115,7 @@ def generate_deps(attributes: 'list[str]') -> 'list[FunctionalDependency]':
   for lhs_attrs in lhs_combos:
     for rhs_attr in attrs:
       if rhs_attr not in lhs_attrs:
-        deps.append((lhs_attrs, rhs_attr))
+        deps.append(FunctionalDependency(lhs_attrs, rhs_attr))
 
   return deps
 
@@ -131,19 +128,19 @@ spark = SparkSession.builder.appName("FuncD").getOrCreate()
 # This reflects the SQL schema of the `users` table provided at:
 # https://github.com/gousiosg/github-mirror/blob/3d5f4b2ffa5d510455e58b1209c31f4d1b211306/sql/schema.sql
 schema = StructType([
-    StructField("id", IntegerType()),
-    StructField("login", StringType()),
-    StructField("company", StringType()),
-    StructField("created_at", TimestampType()),
-    StructField("type", StringType()),
-    StructField("fake", IntegerType()),
-    StructField("deleted", IntegerType()),
-    StructField("long", DecimalType(11, 8)), # numbers define precision
-    StructField("lat", DecimalType(10, 8)), # numbers define precision
-    StructField("country_code", StringType()),
-    StructField("state", StringType()),
-    StructField("city", StringType()),
-    StructField("location", StringType()),
+  StructField("id", IntegerType()),
+  StructField("login", StringType()),
+  StructField("company", StringType()),
+  StructField("created_at", TimestampType()),
+  StructField("type", StringType()),
+  StructField("fake", IntegerType()),
+  StructField("deleted", IntegerType()),
+  StructField("long", DecimalType(11, 8)), # numbers define precision
+  StructField("lat", DecimalType(10, 8)), # numbers define precision
+  StructField("country_code", StringType()),
+  StructField("state", StringType()),
+  StructField("city", StringType()),
+  StructField("location", StringType()),
 ])
 users = spark.read.csv(USERS_DATA_PATH, schema, nullValue='\\N')
 
@@ -164,14 +161,13 @@ def get_country_name(code: str):
 # Add column "country" with the country name based on the country code.
 users = users.withColumn('country', get_country_name(users.country_code))
 
-# %% Compute FDs
+# %% Check FDs
 candidate_deps = generate_deps(users.columns)
 results = []
+for fd in candidate_deps:
+  print(f'Checking FS: {fd.lhs} -> {fd.rhs}')
 
-# %% Check FDs
-for (lhs_attrs, rhs_attr) in candidate_deps:
-  print(f'Checking FS: {lhs_attrs} -> {rhs_attr}')
-  p = dependency_prob(lhs_attrs, rhs_attr)
+  p = dependency_prob(fd)
 
   classification = 'No FD'
   if p == 1:
@@ -180,7 +176,7 @@ for (lhs_attrs, rhs_attr) in candidate_deps:
     classification = 'Soft'
 
   print(f'Probability = {p}, {classification}')
-  results.append([lhs_attrs, rhs_attr, p, classification])
+  results.append([fd.lhs, fd.rhs, p, classification])
 
 # %% Write results
 with open('brute_force_results.csv', mode='w') as file:
