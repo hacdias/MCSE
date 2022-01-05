5c56d165b8984c8119f22b1b61ffc889c248a85f
Henrique Dias <hacdias@gmail.com>
2022-01-05 23:38:16 +0100

    remove leshan server

--- server/leshan-server-core-demo/src/main/java/org/eclipse/leshan/server/core/demo/cli/converters/ServerCIDConverter.java
+++
@@ -1,9 +0,0 @@
-package org.eclipse.leshan.server.core.demo.cli.converters;
-
-import org.eclipse.leshan.core.demo.cli.converters.CIDConverter;
-
-public class ServerCIDConverter extends CIDConverter {
-    public ServerCIDConverter() {
-        super(6);
-    }
-};
