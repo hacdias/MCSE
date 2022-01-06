ed0274dc91400a406072dd4d74d2bfdd88bbfdf7
Henrique Dias <hacdias@gmail.com>
2022-01-06 09:05:42 +0100

    only show vehicle id if spot is not 'free'

--- webapp/src/ParkingSpot.js
+++ webapp/src/ParkingSpot.js
@@ -7,9 +7,9 @@ export default function ParkingSpot({ id, state, vehicle }) {
 
   return (
     <div className={`pa2 ba b--${color} mt2`}>
-      <span className={`w1 h1 mr1 dib bg-${color} br-100 v-mid`}></span>
+      <span className={`w1 h1 mr1 dib bg-${color} br-100 v-mid`} alt={state} title={state}></span>
       id: <code>{id}</code>
-      {vehicle ? ` [${vehicle}]` : null}
+      {vehicle && color !== 'green' ? ` [${vehicle}]` : null}
     </div>
   )
 }
