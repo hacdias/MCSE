d526122e694d2faac61c9273cb7194abf94b23f1
Henrique Dias <hacdias@gmail.com>
2022-01-11 21:53:51 +0100

    filter by coordinates

--- webapp/src/example-data.js
+++ webapp/src/example-data.js
@@ -7,27 +7,63 @@ const exampleData = {
       capacity: 3,
       reservations: 1,
       vehicles: 1,
+      free: 1,
       parkingSpots: [
         {
           id: 'ps-1',
-          state: 'Occupied',
-          vehicle: 'AA-BB-11',
-          x: 3,
+          state: 'Free',
+          vehicle: '',
+          x: 1,
           y: 4
         },
         {
           id: 'ps-2',
+          state: 'Occupied',
+          vehicle: 'AA-BB-11',
+          x: 2,
+          y: 4
+        },
+        {
+          id: 'ps-3',
           state: 'Free',
           vehicle: '',
           x: 3,
           y: 4
         },
         {
-          id: 'ps-3',
-          state: 'Reserved',
-          vehicle: '11-CC-22',
-          x: 3,
+          id: 'ps-5',
+          state: 'Free',
+          vehicle: '',
+          x: 4,
           y: 4
+        },
+        {
+          id: 'ps-6',
+          state: 'Free',
+          vehicle: '',
+          x: 1,
+          y: 3
+        },
+        {
+          id: 'ps-7',
+          state: 'Free',
+          vehicle: '',
+          x: 2,
+          y: 3
+        },
+        {
+          id: 'ps-8',
+          state: 'Free',
+          vehicle: '',
+          x: 3,
+          y: 3
+        },
+        {
+          id: 'ps-9',
+          state: 'Reserved',
+          vehicle: '11-22-AA',
+          x: 4,
+          y: 3
         }
       ],
       vehicleCounters: [
